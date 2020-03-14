72f253c70bed2e62d9a1fa76225f4960884473e6
Łukasz Dziedziul <l.dziedziul@gmail.com>
2020-03-14 20:02:50 +0100

    Create script changing java version in the current shell

---
+++ j.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+SCRIPT_NAME=$0
+
+function print_usage() {
+    VERSIONS=$(ls $SDKMAN_DIR/candidates/java | grep -v current | awk -F'.' '{print $1}' | sort -nr | uniq)
+    CURRENT=$(basename $(readlink $SDKMAN_DIR/candidates/java/current) | awk -F'.' '{print $1}')
+    echo "Available versions: "
+    echo "$VERSIONS"
+    echo "Current: $CURRENT"
+    echo "Usage: j <java_version>"
+}
+
+if [[ $# -eq 1 ]]; then
+  VERSION_NUMBER=$1
+  IDENTIFIER=$(ls $SDKMAN_DIR/candidates/java | grep -v current | grep "$VERSION_NUMBER.0." | sort -r | head -n 1)
+  sdk use java $IDENTIFIER
+else
+  print_usage
+fi
