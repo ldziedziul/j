40fa330253dfec3e7115d66cfcad3ca899121216
Marcus Fihlon <marcus@fihlon.swiss>
2023-04-06 12:05:21 +0200

    🐛 Fix bug by ignoring command aliases

--- j.sh
+++ j.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 function print_usage() {
-    VERSIONS=$(ls $SDKMAN_DIR/candidates/java | grep -v current | awk -F'.' '{print $1}' | sort -nr | uniq)
-    CURRENT=$(basename $(readlink $JAVA_HOME || echo $JAVA_HOME) | awk -F'.' '{print $1}')
-    echo "Available versions: "
-    echo "$VERSIONS"
-    echo "Current: $CURRENT"
-    echo "Usage: j <java_version>"
+    VERSIONS=$(\ls $SDKMAN_DIR/candidates/java | \grep -v current | \awk -F'.' '{print $1}' | \sort -nr | \uniq)
+    CURRENT=$(\basename $(\readlink $JAVA_HOME || \echo $JAVA_HOME) | \awk -F'.' '{print $1}')
+    \echo "Available versions: "
+    \echo "$VERSIONS"
+    \echo "Current: $CURRENT"
+    \echo "Usage: j <java_version>"
 }
 
 if [[ $# -eq 1 ]]; then
   VERSION_NUMBER=$1
-  IDENTIFIER=$(ls $SDKMAN_DIR/candidates/java | grep -v current | grep "^$VERSION_NUMBER." | sort -r | head -n 1)
+  IDENTIFIER=$(\ls $SDKMAN_DIR/candidates/java | \grep -v current | \grep "^$VERSION_NUMBER." | \sort -r | \head -n 1)
   sdk use java $IDENTIFIER
 else
   print_usage
